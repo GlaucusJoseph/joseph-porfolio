82162fdd5e341e4865daf1f2a93f1efb7347e8e9
SirGlaucus <glaucusjoseph@gmail.com>
2023-08-12 14:21:27 -0400

    Going back to basics

--- src/components/MainNavbar/MainNavbar.tsx
+++ src/components/MainNavbar/MainNavbar.tsx
@@ -4,25 +4,11 @@ export const MainNavbar = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-new-style">
       <div className="container-fluid">
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div
-          className="collapse navbar-collapse justify-content-center"
-          id="navbarSupportedContent"
-        >
+        <div className="justify-content-center" id="navbarSupportedContent">
           <ul className="navbar-nav mb-2 mb-lg-0 text-center">
             <li className="nav-item mx-5">
               <a className="link-style" aria-current="page" href="#">
-                Home
+                About me
               </a>
             </li>
             <li className="nav-item">
