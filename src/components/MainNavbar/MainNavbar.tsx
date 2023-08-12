6a3099afab817c85ac794ed49243681692359a12
SirGlaucus <glaucusjoseph@gmail.com>
2023-08-12 14:32:40 -0400

    Changing size and position between mobile and desktop

--- src/components/MainNavbar/MainNavbar.tsx
+++ src/components/MainNavbar/MainNavbar.tsx
@@ -2,29 +2,12 @@ import "./MainNavbar.css";
 
 export const MainNavbar = () => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-new-style">
-      <div className="container-fluid">
-        <div className="justify-content-center" id="navbarSupportedContent">
-          <ul className="navbar-nav mb-2 mb-lg-0 text-center">
-            <li className="nav-item mx-5">
-              <a className="link-style" aria-current="page" href="#">
-                About me
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="link-style" aria-current="page" href="#experience">
-                Experience
-              </a>
-            </li>
-            <li className="nav-item mx-5">
-              <a className="link-style" aria-current="page" href="#projects">
-                Projects
-              </a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
+    <div className="top-div">
+      <p>About me</p>
+      <p>Experience</p>
+      <p>Projects</p>
+      <p>Courses</p>
+    </div>
   );
 };
 
