060ae80535cb8bedff319fa9e8c488ce0a05ce4c
SirGlaucus <glaucusjoseph@gmail.com>
2023-09-04 17:47:13 -0400

    Simplify routes

---
+++ src/components/index.tsx
@@ -0,0 +1,7 @@
+import MainNavbar from "./MainNavbar";
+import About from "./About";
+import Technologies from "./Technologies";
+import WorkExperience from "./WorkExperience";
+import Courses from "./Courses";
+
+export { MainNavbar, About, Technologies, WorkExperience, Courses };
