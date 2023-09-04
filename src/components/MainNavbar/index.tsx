67ef2d1be4419ee22cb30d093ef65f8221fd05dd
SirGlaucus <glaucusjoseph@gmail.com>
2023-09-04 17:48:09 -0400

    New border shadow for the navbar and position/size

--- src/components/MainNavbar/index.tsx
+++ src/components/MainNavbar/index.tsx
@@ -2,11 +2,13 @@ import "./index.css";
 
 const MainNavbar = () => {
   return (
-    <div className="top-div border-shadow">
-      <a href="#about">About me</a>
-      <a href="#experience">Experience</a>
-      <a href="#projects">Projects</a>
-      <a href="#courses">Courses</a>
+    <div className="top-div about-border-shadow">
+      <div className="top-div-content">
+        <a href="#about">About me</a>
+        <a href="#experience">Experience</a>
+        <a href="#projects">Projects</a>
+        <a href="#courses">Courses</a>
+      </div>
     </div>
   );
 };
