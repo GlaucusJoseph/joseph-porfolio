0fc8a9d11d52b184f31e9cb0189a66d0d2317f3b
SirGlaucus <glaucusjoseph@gmail.com>
2023-09-04 20:11:53 -0400

    Adding new section

--- src/components/Courses/index.tsx
+++ src/components/Courses/index.tsx
@@ -1,83 +1,41 @@
-import platziLogo from "../../assets/img/platzilogo.webp";
-import {
-  CypressIcon,
-  GitIcon,
-  GithubIcon,
-  JavaScriptIcon,
-  MeteorIcon,
-  MongoDBIcon,
-  NodeJSIcon,
-  ReactIcon,
-  TypeScriptIcon,
-} from "../../assets/svg";
+import { COURSES_OBJECT_LIST } from "../../constants";
 import "./index.css";
 
 const Courses = () => {
-  const iconsList = [
-    JavaScriptIcon,
-    NodeJSIcon,
-    ReactIcon,
-    TypeScriptIcon,
-    MeteorIcon,
-    MongoDBIcon,
-    CypressIcon,
-    GitIcon,
-    GithubIcon,
-  ];
-
   return (
     <section id="courses">
       <div className="container curses-box">
         <h3 className="my-2">Courses</h3>
         <div className="animated-line" />
         <div className="row">
-          <div className="col-12 col-md-6 work-section">
-            <div className="row">
-              <div className="col-12">
-                <img src={platziLogo} alt="Platzi-logo" width="250" />
-              </div>
-              <div className="col-12">
-                <h5>Docmovi</h5>
-              </div>
-              <div className="col-12">
-                <h6>Full Stack Developer</h6>
-              </div>
-              <div className="col-12">
-                <label>July, 2022 - Present</label>
-              </div>
-              <div className="col-12 mt-2">
-                <div className="courses-icons">
-                  {iconsList.map((Icon, index) => (
-                    <div className="courses-icon-margin" key={index}>
-                      <Icon width="30" height="30" />
+          {COURSES_OBJECT_LIST.map((course) => {
+            return (
+              <div className="col-12 col-md-6 course-box-size">
+                <a href={course.url} target="_blank" rel="noopener noreferrer">
+                  <div className="row">
+                    <div className="col-12">
+                      <h6>
+                        {course.institution} /&nbsp;
+                        <span className="special-text-color">
+                          {course.title}
+                        </span>
+                      </h6>
                     </div>
-                  ))}
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="col-12 col-md-6 work-section">
-            <div className="row">
-              <div className="col-12">
-                <img src={platziLogo} alt="Docmovi-logo" width="250" />
-              </div>
-              <div className="col-12">
-                <h6>Full Stack Developer</h6>
-              </div>
-              <div className="col-12">
-                <label>July, 2022 - Present</label>
-              </div>
-              <div className="col-12 mt-2">
-                <div className="courses-icons">
-                  {iconsList.map((Icon, index) => (
-                    <div className="courses-icon-margin" key={index}>
-                      <Icon width="30" height="30" />
+                    <div className="col-12">{course.date}</div>
+                    <div className="col-12">
+                      <div className="courses-icons">
+                        {course.technologies.map((Icon, index) => (
+                          <div key={index} className="courses-icon-margin">
+                            <Icon width="30" height="30" />
+                          </div>
+                        ))}
+                      </div>
                     </div>
-                  ))}
-                </div>
+                  </div>
+                </a>
               </div>
-            </div>
-          </div>
+            );
+          })}
         </div>
       </div>
     </section>
