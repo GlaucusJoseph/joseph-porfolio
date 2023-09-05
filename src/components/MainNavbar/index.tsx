9b88be53d70a0b95855caff661ca8ced2863800c
SirGlaucus <glaucusjoseph@gmail.com>
2023-09-05 18:23:14 -0400

    Fixing navbar

--- src/components/MainNavbar/index.tsx
+++ src/components/MainNavbar/index.tsx
@@ -4,10 +4,8 @@ const MainNavbar = () => {
   return (
     <div className="top-div about-border-shadow">
       <div className="top-div-content">
-        <a href="#about">About me</a>
-        <a href="#experience">Experience</a>
-        <a href="#projects">Projects</a>
-        <a href="#courses">Courses</a>
+        <a href="#about">About me</a> |<a href="#experience">Experience</a> |
+        <a href="#projects">Projects</a> |<a href="#courses">Courses</a>
       </div>
     </div>
   );
