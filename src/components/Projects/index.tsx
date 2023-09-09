f91d44d3b9b87aa0f728941ad4294299f59f3ee4
SirGlaucus <glaucusjoseph@gmail.com>
2023-09-09 11:27:31 -0400

    New card style

--- src/components/Projects/index.tsx
+++ src/components/Projects/index.tsx
@@ -3,39 +3,37 @@ import "./index.css";
 
 const Projects = () => {
   return (
-    <section id="courses">
+    <section id="projects">
       <div className="container projects-box">
         <h3 className="my-2">Projects</h3>
         <div className="animated-line" />
-        <div className="row">
-          {COURSES_OBJECT_LIST.map((course) => {
-            return (
-              <div className="col-12 col-md-6 course-box-size">
-                <a href={course.url} target="_blank" rel="noopener noreferrer">
-                  <div className="row">
-                    <div className="col-12">
-                      <h6>
-                        {course.institution} /&nbsp;
-                        <span className="special-text-color">
-                          {course.title}
-                        </span>
-                      </h6>
+        <div className="container">
+          <div className="row row-cols-1 row-cols-md-3 g-0 justify-content-center">
+            {COURSES_OBJECT_LIST.map((course) => {
+              return (
+                <div className="card">
+                  <img
+                    className="card-image"
+                    src="https://images.unsplash.com/photo-1557177324-56c542165309?ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80"
+                    alt="Descripción de la imagen"
+                  />
+                  <div className="card-content">
+                    <div className="card-title">
+                      <h5>Projecto Generico Formulario</h5>
                     </div>
-                    <div className="col-12">{course.date}</div>
-                    <div className="col-12">
-                      <div className="courses-icons">
-                        {course.technologies.map((Icon, index) => (
-                          <div key={index} className="courses-icon-margin">
-                            <Icon width="30" height="30" />
-                          </div>
-                        ))}
-                      </div>
+                    <div className="card-description">
+                      <p>Texto de la tarjeta que se superpondrá a la imagen.</p>
                     </div>
+                    <div className="card-tags">
+                      <p>Texto de la tarjeta que se superpondrá a la imagen.</p>
+                    </div>
+                    <button className="card-button-github">GitHub</button>
+                    <button className="card-button-website">Website</button>
                   </div>
-                </a>
-              </div>
-            );
-          })}
+                </div>
+              );
+            })}
+          </div>
         </div>
       </div>
     </section>
