432fa852ca817534a9cea749467b6cea8d47bf88
SirGlaucus <glaucusjoseph@gmail.com>
2023-09-16 18:58:29 -0400

    Change scroll behavior

--- src/components/MainNavbar/index.tsx
+++ src/components/MainNavbar/index.tsx
@@ -1,6 +1,41 @@
+import React, { useEffect } from "react";
 import "./index.css";
 
 const MainNavbar = () => {
+  const handleClick = (
+    e: React.MouseEvent<HTMLAnchorElement>,
+    targetId: string
+  ) => {
+    e.preventDefault();
+    const targetElement = document.getElementById(targetId);
+    if (targetElement) {
+      const navbar = document.querySelector(".navbar-box") as HTMLElement;
+      if (navbar) {
+        const navbarHeight = navbar.offsetHeight;
+        const targetPosition = targetElement.offsetTop - navbarHeight;
+        window.scrollTo({
+          top: targetPosition,
+          behavior: "smooth",
+        });
+      }
+    }
+  };
+
+  useEffect(() => {
+    const links = document.querySelectorAll(".navbar-box-content a");
+    links.forEach((link) => {
+      const targetId = link.getAttribute("href")?.substring(1);
+      if (targetId) {
+        link.addEventListener("click", function (e) {
+          handleClick(
+            e as unknown as React.MouseEvent<HTMLAnchorElement>,
+            targetId
+          );
+        });
+      }
+    });
+  }, []);
+
   return (
     <nav
       className="navbar-box navbar-border-shadow"
