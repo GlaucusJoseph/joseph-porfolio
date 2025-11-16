4d5a7ae9a1a6b2ca43e73a9885fdec50fef4aa8b
Joseph <glaucusjoseph@gmail.com>
2025-11-15 23:18:26 -0300

    temporal fix no projects

--- src/components/Projects/index.tsx
+++ src/components/Projects/index.tsx
@@ -19,7 +19,7 @@ const Projects = () => {
         >
           <div className="container">
             <div className="row row-cols-1 row-cols-md-3 g-0 justify-content-center">
-              {PROJECTS_OBJECT_LIST.map((project, index) => {
+              {PROJECTS_OBJECT_LIST.map((_project, index) => {
                 return (
                   <div className="card" key={index}>
                     <img
